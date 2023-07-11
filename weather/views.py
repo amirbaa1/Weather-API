725768bbf88d8371ee7c6d56d6b4bca4cb38688b
amirbaa1 <amir.baahklag@yahoo.com>
2023-07-11 22:52:07 +0330

    weather html

--- weather/views.py
+++ weather/views.py
@@ -10,10 +10,14 @@ def HomeWeather(request):
         api_key = '690086498ed6ebaabb81343f0cf12fb0'
         city_weather = requests.get(url.format(city_name, api_key)).json()
         print('---->', city_weather)
+        weather_c = round(city_weather['main']['temp'] - 273, 2)
         weather = {
             'city': city_name,
-            'temperature': city_weather['main']['temp'],
+            'country': city_weather['sys']['country'],
+            'temperature': weather_c,
             'description': city_weather['weather'][0]['description'],
             'icon': city_weather['weather'][0]['icon']
         }
         return render(request, 'home.html', context={'weather': weather})
+    # if request.method == 'POST':
+
